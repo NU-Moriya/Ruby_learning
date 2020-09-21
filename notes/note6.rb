ac6c271d8d7f16431d7131bb93892233afda3d63
nobuko.m <animopara100pre@gmail.com>
2020-09-21 14:43:35 +0900

    edited_pattern

--- notes/note6.rb
+++ notes/note6.rb
@@ -8,3 +8,19 @@ elsif n[1] >= 30
   minute = (n[1]+30) - 60
   puts format("%02d:%02d", hour, minute)
 end
+
+
+
+
+n = gets.split(":").map(&:to_i)
+
+hour = n[0]
+result_minute = n[1] + 30
+
+if result_minute < 60
+  puts format("%02d:%02d", hour, result_minute)
+else
+  hour += 1
+  result_minute -= 60
+  puts format("%02d:%02d", hour, result_minute)
+end
