0110dd45841d33573a25168e0035991d58f2287d
nobuko.m <animopara100pre@gmail.com>
2020-10-12 11:08:04 +0900

    add new method

--- notes/note11.rb
+++ notes/note11.rb
@@ -15,4 +15,19 @@ words.each do |i|
 print new
 end
 
+#小数点までの割り算から四捨五入のround
 
+scores = gets.split(" ").map(&:to_i)
+
+sum = 0
+scores.each do |i|
+    sum += i
+end
+average = sum / 7.to_f
+puts average.round(1)
+
+#整数で指定の桁から切り捨てたい場合(下二桁の切り捨て)
+
+price = gets.to_i
+
+puts price.floor(-2)
